55caf200f10060765c4649cecb271fba434267f4
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-29 18:38:43 +0100

    Added missing files

---
+++ PasswordManager.App/Modules/AppModule.cs
@@ -0,0 +1,24 @@
+﻿using DNI.Modules.Shared.Abstractions.Builders;
+using DNI.Modules.Shared.Base;
+using Microsoft.Extensions.DependencyInjection;
+using DNI.Modules.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager.Client;
+using PasswordManager.Server;
+
+namespace PasswordManager.App.Modules
+{
+    public class AppModule : ModuleBase
+    {
+        public override void ConfigureModuleBuilder(IServiceCollection services, IModuleConfigurationBuilder moduleConfigurationBuilder)
+        {
+            moduleConfigurationBuilder
+                .AddModule<ClientModule>()
+                .AddModule<ServerModule>();
+        }
+    }
+}
---
+++ PasswordManager.App/Startup.cs
@@ -0,0 +1,36 @@
+﻿using DNI.Modules.Shared.Abstractions;
+using DNI.Shared.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PasswordManager.App
+{
+    public class Startup : DisposableStartupBase
+    {
+        private readonly IModuleStartup moduleStartup;
+
+        public Startup(IModuleStartup moduleStartup)
+        {
+            this.moduleStartup = moduleStartup;
+        }
+
+        public override void Dispose(bool disposing)
+        {
+            moduleStartup.Dispose();
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken, params object[] args)
+        {
+            return moduleStartup.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return moduleStartup.StopAsync(cancellationToken);
+        }
+    }
+}
