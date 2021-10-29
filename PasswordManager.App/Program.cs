6b344bdb4e7bd7c0b1c938960cd16063d14e8ad1
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-29 18:35:22 +0100

    Implemented infrastructure

--- PasswordManager.App/Program.cs
+++ PasswordManager.App/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using DNI.Core.Defaults.Hosts;
+using DNI.Extensions;
+using DNI.Modules.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PasswordManager.App.Modules;
+using System;
+using System.Threading.Tasks;
 
 namespace PasswordManager.App
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using var consoleHost = ConsoleHost
+                .Build(build => build
+                .ConfigureServices<Startup>(ConfigureServices)
+                .AddDefaultConfiguration());
+
+            await consoleHost.StartAsync(args: args);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddLogging(c => c.AddConsole())
+                .AddModules(builder => builder
+                    .AddModule<AppModule>());
         }
     }
 }
--- Server/PasswordManager.Server/ServerModule.cs
+++ Server/PasswordManager.Server/ServerModule.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using DNI.Mapper.Modules.Extensions;
+using DNI.Mediator.Modules.Extensions;
+using DNI.Modules.Shared.Abstractions.Builders;
+using DNI.Modules.Shared.Base;
+using DNI.Web.Modules.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PasswordManager.Server.Core.Features.Account;
+using System;
 
 namespace PasswordManager.Server
 {
-    public class ServerModule
+    public class ServerModule : ModuleBase
     {
+        public override void ConfigureModuleBuilder(IServiceCollection services, IModuleConfigurationBuilder moduleConfigurationBuilder)
+        {
+            moduleConfigurationBuilder
+                .ConfigureMapperModule(builder => builder
+                    .AddAssembly<AccountController>())
+                .ConfigureMediatorModule(builder => builder
+                    .AddAssembly<AccountController>())
+                .ConfigureWebModule<ServerModule>(builder => builder
+                    .AddAssembly<AccountController>()
+                    .ConfigureWebHost(ConfigureWebHost));
+        }
+
+        private void ConfigureWebHost(IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder.PreferHostingUrls(true)
+                .UseUrls("http://localhost:5600");
+        }
     }
 }
